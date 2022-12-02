aa2abed1c9a1e3f09dc2c3450ffa12b94a805ee7
Levi Francis <levifrancis15@gmail.com>
2022-12-02 21:33:10 +0100

    feat: onboarding section

---
+++ components/OnboardingSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OnboardingSection = () => {
+  return (
+    <div>
+        <main>
+            
+        </main>
+    </div>
+  )
+}
+
+export default OnboardingSection
