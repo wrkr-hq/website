bab78b813f9087efe7e647b87100c27c9dec5806
Levi Francis <levifrancis15@gmail.com>
2022-12-02 23:29:58 +0100

    feat: create onboarding and verify claim sections

--- components/OnboardingSection.jsx
+++ components/OnboardingSection.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import e2e from '../public/endToEnd.svg'
 
 const OnboardingSection = () => {
   return (
     <div>
-        <main>
-            
+        <main className='max-w-[1440px] mx-auto bg-[url("../public/vector2.png")] bg-no-repeat bg-right bg-half'>
+
+            <div className='flex flex-col'>
+              <div className='flex self-center mx-auto'>
+                <div>PortableOS</div>
+                <div>Onboarding</div>
+                <div>Authentication</div>
+                <div>Claims</div>
+                <div>Fraud</div>
+              </div>
+
+              <div className='flex  items-center h-screen pl-[160px]'>
+                <div className='w-[498px] mr-[100px]'>
+                  <div className='text-3xl font-semibold'>End-to-End identity verification for onboarding in 10 seconds</div>
+                  <div>
+                  Reduce friction and abandonment rates at a fragment of legacy system costs, through fully compliant pre-approved KYC from trusted companies within our Identity network, consented and controlled by the customer in under 10 seconds.
+                  </div>
+                </div>
+
+                <div>
+                  <Image 
+                    src={e2e}
+                    alt="end-to-end encryption"
+                    width={306}
+                    height={495} />
+                </div>
+              </div>
+            </div>
         </main>
     </div>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navigation from './Navigation'
 import Landing from './Landing'
+import OnboardingSection from '../components/OnboardingSection'
+import VerifyClaimSection from '../components/VerifyClaimSection'
 
 export default function Home() {
   return (
@@ -13,6 +15,13 @@ export default function Home() {
       <main>
         <Navigation/>
         <Landing/>
+        <div>
+        <OnboardingSection />
+        </div>
+
+        <div>
+          <VerifyClaimSection />
+        </div>
       </main>
       <footer className={styles.footer}>
       </footer>
