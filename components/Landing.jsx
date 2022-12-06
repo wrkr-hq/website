5566e47eb7ba510b52f0dd314b8aafb14e69b41f
Chloe <selzingmusa1@gmail.com>
2022-12-06 15:35:06 +0100

    chore:Add landing section background image

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import home from '../public/home.svg'
-import overlay from '../public/overlay.svg'
+import landing from '../public/landing.png'
 import Image from 'next/image'
 const Landing = () => {
   return (
 <div className='bg-[#02000B] pt-20 lg:pt-[151px]'>
  {/* <Image src={overlay} alt='/' className='bg-black '  /> */}
-<div className='flex flex-col-reverse w-full px-10 lg:flex-row max-w-[1440px] mx-auto justify-between lg:px-20 py-14 text-white'>
+<div className='flex flex-col-reverse w-full px-10 lg:flex-row lg:bg-[url("../public/Group.svg")] lg:bg-no-repeat max-w-[1440px] mx-auto justify-between lg:px-20 py-14 text-white'>
         <div>
             <h1 className='text-xl lg:text-4xl font-bold pb-6 lg:pb-14'> 
             The Portable Way of Proving <br />
@@ -25,7 +24,7 @@ const Landing = () => {
             </button>
         </div>
         <div className='place-content-center'>
-          <Image  src={home} alt='/' />
+          <Image  src={landing} alt='/' />
         </div>
     </div>
 
